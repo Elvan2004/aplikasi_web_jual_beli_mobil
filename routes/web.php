08c132acecceecbdeec9bb8b173eb3344e29f4d4
Elvan 3312311004 <elvanlee2004@gmail.com>
2024-03-04 14:19:25 +0700

    Update web.php dan penambahan file baru ListBarangController.php dan list_barang.blade.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ListBarangController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
 });
+
+Route::get('/user/{id}', function ($id) {
+    return 'User dengan ID ' . $id;
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return 'Admin Dashboard';
+    });
+
+    Route::get('/users', function () {
+        return 'Admin Users';
+    });
+});
+
+// Route::get('/listbarang/{id}/{nama}', function ($id, $nama) {
+//     return view('list_barang', compact('id', 'nama'));
+// });
+
+Route::get('/listbarang/{id}/{nama}', [ListBarangController::class, 'tampilkan']);
