595519f1e9a63d8ac4635682251e8f1e0b832b59
Elvan 3312311004 <elvanlee2004@gmail.com>
2024-03-05 13:52:46 +0700

    Praktikum 4 - Mencetak data dengan looping. File : ListBarangController.php dan list_barang.blade.php

--- resources/views/list_barang.blade.php
+++ resources/views/list_barang.blade.php
@@ -1,7 +1,28 @@
 <html>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nama</th>
+                <th>Harga</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($data as $dataku)
+                <tr>
+                    <td>{{ $dataku['id'] }}</td>
+                    <td>{{ $dataku['nama'] }}</td>
+                    <td>{{ $dataku['harga'] }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</html>
+
+{{-- <html>
     <div>
         <h1>List Barang</h1>
         <p>Kode Barang : {{$id}}</p>
         <p>Nama Barang : {{$nama}}</p>
     </div>
-</html>
+</html> --}}
