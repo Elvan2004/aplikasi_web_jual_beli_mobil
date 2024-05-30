2a8ec53c429fbb245f48d31a7742757efe351a9d
Elvan 3312311004 <elvanlee2004@gmail.com>
2024-05-30 20:12:22 +0700

    Praktikum 11 - API

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiController;
+
+Route::get('api/produk', [ApiController::class, 'index']);
+Route::get('api/list', [ApiController::class, 'getProduct']);
 
 /*
 |--------------------------------------------------------------------------
